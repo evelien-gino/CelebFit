4af9f7cc68ebde11171b9969ea4df7decf957b04
evelien-gino <evelien.gino.s95@kalvium.community>
2025-11-19 10:50:36 +0530

    first commit

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-// backend/server.js
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// connect to MongoDB
-const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/celebfitsample';
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(()=> console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB error', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
 
-// Simple route
-app.get('/', (req, res) => res.send('CelebFit API up'));
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 
-// import routes (we'll create them)
-const celebRoutes = require('./routes/celebrities');
-app.use('/api/celebrities', celebRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
